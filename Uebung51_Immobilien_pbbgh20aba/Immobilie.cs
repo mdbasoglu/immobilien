65b34ca4a513bd913b4258cfaeed99629a76fdfe
mdbasoglu <69213997+mdbasoglu@users.noreply.github.com>
2021-12-13 17:57:44 +0100

    erstes Commit

---
+++ Uebung51_Immobilien_pbbgh20aba/Immobilie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Uebung51_Immobilien_pbbgh20aba
+{
+    class Immobilie
+    {
+
+        //Attribute
+        private string beschreibung;
+        private double wohnflaeche;
+        private int anzahlZimmer;
+        private int baujahr;
+        private double kaufpreis;
+        private bool verkauft;
+
+        public Immobilie(string beschreibung, double wohnflaeche, int anzahlZimmer, int baujahr, double kaufpreis, bool verkauft)
+        {
+            this.beschreibung = beschreibung;
+            this.wohnflaeche = wohnflaeche;
+            this.anzahlZimmer = anzahlZimmer;
+            this.baujahr = baujahr;
+            this.kaufpreis = kaufpreis;
+            this.verkauft = verkauft;
+        }
+
+
+        //Ekrana yazdirmak icin kullaniyoruz yani detaylandirmak icin bicim denebilir
+        public override string ToString()
+        {
+            return $"Immobilie [Beschreibung={beschreibung}, wohnfäche={wohnflaeche},anzahlzimmer={anzahlZimmer}]" +
+                $"baujahr={baujahr}, kaufpreis={kaufpreis}, verkauft={verkauft}";
+
+        }
+
+
+    }
+}
---
+++ Uebung51_Immobilien_pbbgh20aba/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Uebung51_Immobilien_pbbgh20aba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Immobilie objekt1 = new Immobilie("Doppelhaushälfte innenstadtnah", 110, 5, 1977, 210000, true);
+            Immobilie objekt2 = new Immobilie("Traumhaus am Waldrand", 165, 8, 2005, 650000, false);
+            Immobilie objekt3 = new Immobilie("Whohnung für Studenten", 60, 2, 1999, 150000, false);
+
+
+            Console.WriteLine(objekt1);
+            Console.WriteLine(objekt2);
+            Console.WriteLine(objekt3);
+            */
+
+
+            Immobilie[] haus = LeseImmobilien();
+            Console.WriteLine(string.Join<Immobilie>(", ", haus));
+
+        }
+
+
+        public static Immobilie[] LeseImmobilien()
+        {
+
+            string[] lines = File.ReadAllLines(@"Daten/Immobillie.csv");
+
+            Immobilie[] immobilie = new Immobilie[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] data = lines[i].Split(',');
+
+                string beschreibung = data[0];
+                double wohnflaeche = Convert.ToDouble(data[1]);
+                int anzahlzimmer = Convert.ToInt32(data[2]);
+                int baujahr = Convert.ToInt32(data[3]);
+                double kaufpreis = Convert.ToDouble(data[4]);
+                bool verkauft = Convert.ToBoolean(data[5]);
+
+
+                immobilie[i] = new Immobilie(data[0], wohnflaeche, anzahlzimmer, baujahr, kaufpreis, verkauft);
+
+            }
+            return immobilie;
+        }
+    }
+}
